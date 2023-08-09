ede8346389d0abf052bcc9cc0e516cec0484fdf6
IrvinFrias <irvin.frias.unam@outlook.com>
2023-08-08 18:23:02 -0600

    Footer created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {SharedViewsModule} from "./shared-views/shared-views.module";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        SharedViewsModule,
+        NgbModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
